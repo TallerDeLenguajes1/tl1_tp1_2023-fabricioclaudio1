35bb344071e9dfc2f53b77a86609e68f778504e1
FabriRoman <fabricioclaudio1@gmail.com>
2023-03-24 19:52:29 -0300

    Segundo commit

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,7 +2,21 @@
 
 int main(){
     
-    printf("Hola mundo");
+    printf("Hola mundo\n");
+
+    int num1=2, *num2;
+
+    num2=&num1;
+
+    printf("Contenido del puntero: %d \n",*num2);
+
+    printf("Direccion de memoria almacenada en el puntero: %d\n", num2);
+
+    printf("Direccion de memoria de la variable:  %d\n", &num1);
+
+    printf("Direccion de memoria del puntero: %d\n", &num2);
+
+    printf("Tamanio de memoria de la variable: %d\n", sizeof(num1));
 
     return 0;
 }
